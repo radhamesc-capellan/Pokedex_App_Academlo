341cd36219cd94c6b8593528da9e6f361550c322
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2023-01-18 23:08:15 -0500

    Tailwind-Css install

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  tailwindConfig: './styles/tailwind.config.js',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
