c72c713be7cede719f5646e3180446a9a04cb63b
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2023-01-19 21:57:46 -0500

    pokedex pages fetch

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Pokedex = () => {
+  const userName = useSelector((state) => state.userName);
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/`) //https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1279
+      .then((res) => setPokemon(res.data.results));
+  }, []);
+
+  console.log(pokemon);
+
   return (
     <div>
       <h1>Ruta de Pokedex</h1>
+      <p>Welcome {userName}</p>
     </div>
   );
 };
