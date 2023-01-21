a9122afd7e62a5dc52014cb7f8625cfba4fe6ce6
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2023-01-20 20:19:54 -0500

    Pokemon Deatil fetch

--- src/components/PokedexDetail.jsx
+++ src/components/PokedexDetail.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const PokedexDetail = () => {
+
+  const {id} = useParams();
+
+  const [pokemonDetail, setpokemonDetail] = useState({});
+
+  useEffect(() => {
+    axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(res => setpokemonDetail(res.data));
+  }, [])
+
+  console.log(pokemonDetail);
+
   return (
     <div>
-      <h1>Ruta de Pokedex Detail</h1>
+      <h1>{pokemonDetail.name}</h1>
+      <h2>{id}</h2>
+      <img src={pokemonDetail.sprites?.front_default} alt="" />
     </div>
   );
 };
